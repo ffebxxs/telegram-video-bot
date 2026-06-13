fd8154e9c6f1f375aa39916b288007f62a08fe35
ffebxxs <Bacerozpebi24@gmail.com>
2026-06-13 19:55:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,20 +38,19 @@ bot = telebot.TeleBot(TOKEN)
 # =========================
 
 def load_videos():
-if not os.path.exists(DB_FILE):
-return {}
+    if not os.path.exists(DB_FILE):
+        return {}
 
-```
-try:
-    with open(DB_FILE, "r", encoding="utf-8") as f:
-        return json.load(f)
-except:
-    return {}
-```
+    try:
+        with open(DB_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except:
+        return {}
+
 
 def save_videos(data):
-with open(DB_FILE, "w", encoding="utf-8") as f:
-json.dump(data, f, ensure_ascii=False, indent=4)
+    with open(DB_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 # =========================
 
